9fee6972bf6a14fc2fdf546145d38ceee07a6fb4
NaziaSiddique <nazia.siddique04@gmail.com>
2025-09-18 07:59:20 +0100

    Exploring get, post, delete methods

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const app = express();
 
-app.use((req, res) => {
-    res.send('Hello, from the express server!');
+app.get("/user", (req, res) => {
+    res.send({ firstName: "Queen", lastName: "Kaelix" });
 });
+
+app.post("/user", (req, res) => {
+    console.log("Save Data to the Database");
+    res.send("Data successfully saved to the Database!");
+});
+
+app.delete("/user", (req, res) => {
+    console.log("Delete Data from the Database");
+    res.send("Data successfully deleted from the Database!");
+});
+
+app.use("/Test", (req, res) => {
+    res.send("Hello from the /Test endpoint!");
+});
+
+// app.use("/", (req, res) => {
+//     res.send('Salaam Naz!');
+// });
 
 app.listen(3005, () => {
     console.log('Server is running on port 3005');
