5b564544abb41e59a3206ed690b94ad4641b8a76
NaziaSiddique <nazia.siddique04@gmail.com>
2025-09-20 10:25:02 +0100

    Checking for authorised and unauthorised users

--- src/app.js
+++ src/app.js
@@ -1,34 +1,25 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
-app.use(
-    "/user",[
-    (req, res, next) => {
-        console.log("Handle the route user!!");
-        // res.send("First Response!!");
-        next();
-    },
-    (req, res, next) => {
-        console.log("Second callback!!");
-        // res.send("Response from second callback!!");
-        next();
-    },
-     (req, res, next) => {
-        console.log("Third callback!!");
-        // res.send("Response from third callback!!");
-        next();
-    },
-     (req, res, next) => {
-        console.log("Fourth callback!!");
-        // res.send("Response from fourth callback!!");
-        next();
-    },
-     (req, res) => {
-        console.log("Fifth callback!!");
-        res.send("Response from fifth callback!!");
-    }]
-);
+app.get("/admin/getAllData", (req, res) => {
+  //Logic checking if the request is authorised
+//   const token = "ABC"; For unauthorised request
+  const token = "xyz"; // For authorised request
+
+  const isAdminAuthorised = token === "xyz";
+  if (isAdminAuthorised) {
+    res.send("All Data Sent!");
+  }
+  else {
+    res.status("401").send("Unauthorised Request!");
+  }
+});
+
+app.get("/admin/deleteUser", (req, res, next) => {
+  //Logic of deleting a user
+  res.send("User Deleted!!");
+});
 
 app.listen(3005, () => {
-    console.log('Server is running on port 3005');
+  console.log("Server is running on port 3005");
 });
