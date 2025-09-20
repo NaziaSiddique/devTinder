d053834b1544451f9191415f7bfb15c6a3ebcc2d
NaziaSiddique <nazia.siddique04@gmail.com>
2025-09-20 07:22:16 +0100

    playing with multiple route handlers and the next ()

--- src/app.js
+++ src/app.js
@@ -1,27 +1,33 @@
 const express = require('express')
 const app = express();
 
-app.get("/user", (req, res) => {
-    res.send({ firstName: "Queen", lastName: "Kaelix" });
-});
-
-app.post("/user", (req, res) => {
-    console.log("Save Data to the Database");
-    res.send("Data successfully saved to the Database!");
-});
-
-app.delete("/user", (req, res) => {
-    console.log("Delete Data from the Database");
-    res.send("Data successfully deleted from the Database!");
-});
-
-app.use("/Test", (req, res) => {
-    res.send("Hello from the /Test endpoint!");
-});
-
-// app.use("/", (req, res) => {
-//     res.send('Salaam Naz!');
-// });
+app.use(
+    "/user",[
+    (req, res, next) => {
+        console.log("Handle the route user!!");
+        // res.send("First Response!!");
+        next();
+    },
+    (req, res, next) => {
+        console.log("Second callback!!");
+        // res.send("Response from second callback!!");
+        next();
+    },
+     (req, res, next) => {
+        console.log("Third callback!!");
+        // res.send("Response from third callback!!");
+        next();
+    },
+     (req, res, next) => {
+        console.log("Fourth callback!!");
+        // res.send("Response from fourth callback!!");
+        next();
+    },
+     (req, res) => {
+        console.log("Fifth callback!!");
+        res.send("Response from fifth callback!!");
+    }]
+);
 
 app.listen(3005, () => {
     console.log('Server is running on port 3005');
